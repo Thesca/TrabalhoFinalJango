cfaf70e3c8db9c1cbe9c3b73164ca01bdb3a8901
Vinicius  Santana <111644554+ViniSantana179@users.noreply.github.com>
2023-05-13 10:55:07 -0300

    Removendo os testes

--- main.cpp
+++ main.cpp
@@ -3,10 +3,5 @@
 using namespace std;
 
 int main () {
-    cout << "Hello World!" << endl;
-
-	
-    cout << "Thiagão deu Boa" << endl;
-
-    cout << "Vinição deu boa" << endl;
+   cout << "Iniciando Projeto"; 	
 }

14048a3713607391446a51803861339c79824c16
Thiago <thiaguescaliante@gmail.com>
2023-05-15 18:37:42 -0300

    Identando e brainstorm, printando a arena do SnakeGame

--- main.cpp
+++ main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
 
+#define widthSnake 40
+#define heightSnake 20
+
 using namespace std;
 
-int main () {
-   cout << "Iniciando Projeto";
+void printLineSnake() {
+   for (int i = 0; i < widthSnake; ++i) {
+      cout << "#";
+   } cout << endl;
+}
 
-   //Shhhh
+//Setup para o jogo snake
+void setupSnakeGame() {
+
+}
+
+//Função para desenhar a arena do snake
+void drawSnake () {
+   printLineSnake();
+
+   for (int i = 0; i < heightSnake; ++i) {
+      for (int j = 0; j < widthSnake; ++j) {
+         if (j == 0 || j == widthSnake-1) {
+            cout << "#";
+         }else {
+            cout<<" ";
+         }
+      }cout<<endl;
+   }
+   printLineSnake();
+}
+
+int main () {
+   
+   drawSnake();
+   //while(!gameOverSnake);
+
+   return 0;
 }

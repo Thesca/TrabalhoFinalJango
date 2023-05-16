6549da43d5035dd8ca9691536db7b2bf2da7834a
Thiago <thiaguescaliante@gmail.com>
2023-05-15 23:49:38 -0300

    Consertei o erro hehe

--- snake.h
+++ snake.h
@@ -1,9 +1,9 @@
 #ifndef SNAKE_H
 #define SNAKE_H
 
-void drawSnake () {}
-void setupSnake() {}
-void inputSnake() {}
-void logicSnake() {}
+void drawSnake ();
+void setupSnake();
+void inputSnake();
+void logicSnake();
 
 #endif
